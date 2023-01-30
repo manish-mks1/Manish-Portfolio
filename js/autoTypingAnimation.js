5baf2261bb75dabd15639d43a7cc7f6f8ac21cc8
Manish Kumar <104042802+Manish-kumar-1@users.noreply.github.com>
2023-01-30 19:43:33 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am MANISH KUMAR</span> from bhagalpur bihar. currently i pursuing M.C.A. with Industry Oriented Specialization in Computer Network and Cyber Security From Galgotias University Greater Noida.<br><br>  Now, I preparing my self in cyber security fields with doing course of ethical hacking from Internshala and I have skills in programming language like c, c++, java, python, Base scripting and web designing skills with html, css, javascript and Bootstrap. I make many projects related to my skills while learing time which make me more experience in this fields."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
