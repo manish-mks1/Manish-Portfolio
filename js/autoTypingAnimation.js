d42c2c5fa358e89d4673b41bdd5354bf218855ab
Manish Kumar <104042802+manish-mks1@users.noreply.github.com>
2024-02-05 18:40:27 +0530

    Update autoTypingAnimation.js

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hi, I am MANISH KUMAR</span> from bhagalpur bihar. currently i pursuing M.C.A. with Industry Oriented Specialization in Computer Network and Cyber Security From Galgotias University Greater Noida.<br><br>  Now, I preparing my self in cyber security fields with doing course of ethical hacking from Internshala and I have skills in programming language like c, c++, java, python, Base scripting and web designing skills with html, css, javascript and Bootstrap. I make many projects related to my skills while learing time which make me more experience in this fields."
+        "<span>Hi, I am MANISH KUMAR</span> Currently i live in Greater Noida UP for pursuing M.C.A. From Galgotias University.<br><br>  Now, I preparing my self as Java Developer with doing course of spring boot and I have skills in Java, DSA and web designing skills with html, css, javascript and Bootstrap. I make many projects related to my skills while learing time which make me more experience in this fields."
       ],
       typeSpeed: 7,
       contentType: 'html'
